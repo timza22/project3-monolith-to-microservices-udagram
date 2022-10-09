29e4a0ac9aea19d6921dbc467d10c16b97fe96a1
Fatou Fall <fatou.c.fall@aims-senegal.org>
2022-10-09 02:57:52 +0000

    add DockerHub CI push

--- docker_push.sh
+++ docker_push.sh
@@ -2,15 +2,15 @@
 
 docker-compose -f docker-compose-build.yaml build
 
-docker tag reverseproxy "${DOCKER_USERNAME}/reverseproxy:v1.2"
-docker tag udagram-api-user "${DOCKER_USERNAME}/udagram-api-user:v1.1"
-docker tag udagram-api-feed "${DOCKER_USERNAME}/udagram-api-feed:v1.1"
-docker tag udagram-frontend:local "${DOCKER_USERNAME}/udagram-frontend:v1.2"
+docker tag reverseproxy "fatoucheikh77/reverseproxy:v1.2"
+docker tag udagram-api-user "fatoucheikh77/udagram-api-user:v1.1"
+docker tag udagram-api-feed "fatoucheikh77/udagram-api-feed:v1.1"
+docker tag udagram-frontend:local "fatoucheikh77/udagram-frontend:v1.2"
 
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+echo FallFatou@77 | docker login --username fatoucheikh77 --password-stdin
 
-docker push "${DOCKER_USERNAME}/reverseproxy:v1.2" &
-docker push "${DOCKER_USERNAME}/udagram-api-user:v1.1" &
-docker push "${DOCKER_USERNAME}/udagram-api-feed:v1.1" &
-docker push "${DOCKER_USERNAME}/udagram-frontend:v1.2" &
+docker push "fatoucheikh77/reverseproxy:v1.2" &
+docker push "fatoucheikh77/udagram-api-user:v1.1" &
+docker push "fatoucheikh77/udagram-api-feed:v1.1" &
+docker push "fatoucheikh77/udagram-frontend:v1.2" &
 wait
